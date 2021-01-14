3ee332852b66e4484c3b3b2d766c0110693c9f6c
Zywy <piotr@zywolewski.eu>
2021-01-14 14:23:58 +0100

    Started styling for common messages

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="include/style.css">
+</head>
+<body>
+    
+    <div class="topStuff">
+        <div class="nav">
+            <div class="navPart">Nowa Wiadomosc</div>
+            <div class="navPart">Wiadomosci</div>
+        </div>
+        <div class="ImportantMessagesDiv">
+            <div class="ImportantMessages">lollllllllllllllllllllllllllll</div>
+            <div class="ImportantMessages">lollllllllllllllllllllllllllll</div>
+            <div class="ImportantMessages">lollllllllllllllllllllllllllll</div>
+            <div class="ImportantMessages">lollllllllllllllllllllllllllll</div>
+            <div class="ImportantMessages">lollllllllllllllllllllllllllll</div>
+        </div>
+    </div>
+    <div class="content">
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+        <div class="messages">text inside messages</div>
+
+    </div>
+
+
+</body>
+</html>
